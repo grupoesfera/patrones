754ef381f6ae527d1f0729907297f63ad52fe9be
Alejandro Giorgi <alejandro.giorgi@grupoesfera.com.ar>
2025-03-06 17:43:24 -0300

    App con metodo main para ejecutar el control remoto

--- src/main/java/ar/com/grupoesfera/comunidad/patrones/App.java
+++ src/main/java/ar/com/grupoesfera/comunidad/patrones/App.java
@@ -10,26 +10,6 @@ public class App {
         final Luz luzCocina = new Luz("Cocina");
         final Televisor televisorDormitorio = new Televisor("Dormitorio");
 
-        final EncenderLuz encenderLuzComedor = new EncenderLuz(luzComedor);
-        final ApagarLuz apagarLuzComedor = new ApagarLuz(luzComedor);
-
-        final EncenderLuz encenderLuzCocina = new EncenderLuz(luzCocina);
-        final ApagarLuz apagarLuzCocina = new ApagarLuz(luzCocina);
-
-        final EncenderTelevisor encenderTelevisorDormitorio = new EncenderTelevisor(televisorDormitorio);
-        final ApagarTelevisor apagarTelevisorDormitorio = new ApagarTelevisor(televisorDormitorio);
-
         ControlRemoto controlRemoto = new ControlRemoto();
-
-        controlRemoto.asignarComando(0, encenderLuzComedor, apagarLuzComedor);
-        controlRemoto.asignarComando(1, encenderLuzCocina, apagarLuzCocina);
-        controlRemoto.asignarComando(2, encenderTelevisorDormitorio, apagarTelevisorDormitorio);
-
-        controlRemoto.presionarBotonDeEncendido(0);
-        controlRemoto.presionarBotonDeEncendido(1);
-
-        controlRemoto.presionarBotonDeApagado(1);
-
-        controlRemoto.presionarBotonDeEncendido(2);
     }
 }
